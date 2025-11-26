6527379c2a21a4c1baae2a09d14242cdaf19de53
doxyii00-ui <doxyii00@gmail.com>
2025-11-26 18:21:16 +0100

    Update create_app.py

--- create_app.py
+++ create_app.py
@@ -1,7 +1,11 @@
+# create_app.py
 from flask import Flask
-from document_routes import document_bp
 
 def create_app():
     app = Flask(__name__)
+
+    # importujemy blueprinty tutaj, po utworzeniu app
+    from document_routes import document_bp
     app.register_blueprint(document_bp)
+
     return app
